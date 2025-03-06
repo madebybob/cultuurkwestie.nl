6c50015caccf13a0c544c4bfbf38b92f9bc0630f
Bob Mulder <info@madebybob.nl>
2025-03-06 06:12:19 +0100

    Initial commit

---
+++ app/themes/salient-child/resources/js/jquery.cultuurkwestie.js
@@ -0,0 +1,9 @@
+(function (jQuery) {
+
+  jQuery( document ).ready(function() {
+    window.setTimeout(function() {
+      var items = jQuery('#fp-nav ul li a[href="#page"]').hide()
+    }, 500)
+  });
+
+})(jQuery)
